7a056f45d55a1fb26c598f619770c4d39b9de704
Marcella Mutschler <marcella.mutschler@whu.edu>
2017-09-05 11:39:53 +0200

    Add task model

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
+  get 'tasks/index'
+
+  get 'tasks/show'
+
+  get 'tasks/new'
+
+  get 'tasks/create'
+
+  get 'tasks/edit'
+
+  get 'tasks/update'
+
+  get 'tasks/complete'
+
+  get 'tasks/destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
